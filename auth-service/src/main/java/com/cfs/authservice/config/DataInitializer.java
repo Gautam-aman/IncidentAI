4090c8691540c21e2d13df6f40107aa5d1505e95
Aman Gautam <amangautam2128@gmail.com>
2026-05-22 00:30:22 +0530

    added command line runner for app start

---
+++ auth-service/src/main/java/com/cfs/authservice/config/DataInitializer.java
@@ -0,0 +1,27 @@
+package com.cfs.authservice.config;
+
+
+import com.cfs.authservice.entity.Role;
+import com.cfs.authservice.entity.RoleName;
+import com.cfs.authservice.repo.RoleRepository;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner {
+	// CommandLineRunner --> automatic run when app start
+	private final RoleRepository roleRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		for(RoleName roleName : RoleName.values()) {
+			roleRepository.findByName(roleName.name())
+					.orElseGet(()->
+							roleRepository.save(Role.builder().name(roleName.name()).build()));
+		}
+	}
+}
