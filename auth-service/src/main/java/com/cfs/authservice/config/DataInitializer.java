0882fb87a369bb91196c8dff5921aee2f133b262
Aman Gautam <amangautam2128@gmail.com>
2026-05-22 00:39:45 +0530

    added password encoder

--- auth-service/src/main/java/com/cfs/authservice/config/DataInitializer.java
+++ auth-service/src/main/java/com/cfs/authservice/config/DataInitializer.java
@@ -6,7 +6,6 @@ import com.cfs.authservice.entity.RoleName;
 import com.cfs.authservice.repo.RoleRepository;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
