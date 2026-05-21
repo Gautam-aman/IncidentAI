bfb2c700576fadb9fcb4516b76223b1a6d4f1f1e
Aman Gautam <amangautam2128@gmail.com>
2026-05-22 00:42:57 +0530

    added jwt utility

---
+++ auth-service/src/main/java/com/cfs/authservice/util/JwtUtil.java
@@ -0,0 +1,75 @@
+package com.cfs.authservice.util;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+import javax.crypto.SecretKey;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.Value;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+
+	@Value("${jwt.secret}")
+	private String secret;
+
+	@Value("${jwt.access-expiration}")
+	private Long accessExpiration;
+
+	private SecretKey secretKey;
+
+	@PostConstruct
+	public void init() {
+		this.secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+	}
+
+	public String generateToken(String subject, Map<String, Object> claims) {
+		return Jwts.builder()
+				.setClaims(claims)
+				.setSubject(subject)
+				.setIssuedAt(new Date())
+				.setExpiration(new Date(System.currentTimeMillis() + accessExpiration))
+				.signWith(secretKey, SignatureAlgorithm.HS256)
+				.compact();
+	}
+
+	public String extractSubject(String token) {
+		return extractClaim(token, Claims::getSubject);
+	}
+
+	public Date extractExpiration(String token) {
+		return extractClaim(token, Claims::getExpiration);
+	}
+
+	public <T> T extractClaim(String token, Function<Claims, T> resolver) {
+		Claims claims = extractAllClaims(token);
+		return resolver.apply(claims);
+	}
+
+	public Boolean isTokenValid(String token, String subject) {
+		final String extractedSubject = extractSubject(token);
+		return extractedSubject.equals(subject) && !isTokenExpired(token);
+	}
+
+	public Boolean isTokenExpired(String token) {
+		return extractExpiration(token).before(new Date());
+	}
+
+	private Claims extractAllClaims(String token) {
+
+		return Jwts.parserBuilder()
+				.setSigningKey(secretKey)
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
+	}
+
+}
