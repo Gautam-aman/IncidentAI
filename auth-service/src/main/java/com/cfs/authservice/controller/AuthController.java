9cb378a10cf48c5de8e4c0151da94e90b9e9e7fe
Aman Gautam <amangautam2128@gmail.com>
2026-05-30 22:05:45 +0530

    Added auth controller

---
+++ auth-service/src/main/java/com/cfs/authservice/controller/AuthController.java
@@ -0,0 +1,47 @@
+package com.cfs.authservice.controller;
+
+import com.cfs.authservice.dto.AuthResponse;
+import com.cfs.authservice.dto.LoginRequest;
+import com.cfs.authservice.dto.MessageResponse;
+import com.cfs.authservice.dto.RefreshTokenRequest;
+import com.cfs.authservice.dto.RegisterRequest;
+import com.cfs.authservice.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+	private final AuthService authService;
+
+	@PostMapping("/register")
+	public MessageResponse registerUser(@Valid @RequestBody RegisterRequest registerRequest){
+		return authService.register(registerRequest);
+	}
+
+	@PostMapping("/login")
+	public AuthResponse login(@Valid @RequestBody LoginRequest loginRequest){
+		return authService.login(loginRequest);
+	}
+
+	@PostMapping("/refresh")
+	public AuthResponse refresh(@Valid @RequestBody RefreshTokenRequest request){
+		return authService.refreshToken(request);
+	}
+
+	@GetMapping("/me")
+	public String currentUser(Authentication authentication){
+		return authentication.getName();
+	}
+
+}
