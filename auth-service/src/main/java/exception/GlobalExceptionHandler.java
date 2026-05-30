47e0f37103f2597ada6b50c65ad27d6e84cf04f3
Aman Gautam <amangautam2128@gmail.com>
2026-05-30 22:07:57 +0530

    Added global exception handler

---
+++ auth-service/src/main/java/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package exception;
+
+import com.cfs.authservice.dto.MessageResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler  {
+
+	public ResponseEntity<MessageResponse> handleException(Exception ex){
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MessageResponse(ex.getMessage()));
+	}
+
+}
