a0cb4c96a4f88566e5b0bbbcbaed60a02eb8ce3c
Aman Gautam <amangautam2128@gmail.com>
2026-05-31 23:10:40 +0530

    added id filter for tracing

---
+++ gateway-service/src/main/java/com/cfs/gatewayservice/filter/CorrelationIdFilter.java
@@ -0,0 +1,32 @@
+package com.cfs.gatewayservice.filter;
+
+
+import java.util.UUID;
+
+import reactor.core.publisher.Mono;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+@Component
+public class CorrelationIdFilter
+		implements GlobalFilter {
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		String correlationId =
+				UUID.randomUUID().toString();
+
+		exchange.getRequest()
+				.mutate()
+				.header(
+						"X-Correlation-ID",
+						correlationId
+				)
+				.build();
+
+		return chain.filter(exchange);
+	}
+}
