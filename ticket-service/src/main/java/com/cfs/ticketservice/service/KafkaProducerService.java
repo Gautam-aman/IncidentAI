9017afdc98f875e0aa4cd0413724a03e0c163fa3
Aman Gautam <amangautam2128@gmail.com>
2026-06-19 18:59:22 +0530

    Added kafka producer service

---
+++ ticket-service/src/main/java/com/cfs/ticketservice/service/KafkaProducerService.java
@@ -0,0 +1,18 @@
+package com.cfs.ticketservice.service;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class KafkaProducerService {
+
+	private final KafkaTemplate<String, Object> kafkaTemplate;
+
+	public void publish(String topic , Object Event){
+		kafkaTemplate.send(topic, Event);
+	}
+
+}
