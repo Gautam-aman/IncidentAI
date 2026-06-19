a8ce3f67416e6f16b00bf06ac7ecfbd7fac42113
Aman Gautam <amangautam2128@gmail.com>
2026-06-19 18:49:25 +0530

    Updated Kafka Config for ticket service

---
+++ ticket-service/src/main/java/com/cfs/ticketservice/config/KafkaTopicConfig.java
@@ -0,0 +1,39 @@
+package com.cfs.ticketservice.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+	@Bean
+	public NewTopic incidentCreateTopic() {
+		return TopicBuilder
+				.name("incident-created")
+				.partitions(3)
+				.replicas(1)
+				.build();
+	}
+
+	@Bean
+	public NewTopic incidentAssignedTopic() {
+		return TopicBuilder
+				.name("incident-assigned")
+				.partitions(3)
+				.replicas(1)
+				.build();
+	}
+
+	@Bean
+	public NewTopic incidentStatusTopic() {
+		return TopicBuilder
+				.name("incident-status-changed")
+				.partitions(3)
+				.replicas(1)
+				.build();
+	}
+
+}
