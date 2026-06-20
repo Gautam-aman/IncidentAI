d05ce4960c2c1bb76033b0212982faf189801b81
Aman Gautam <amangautam2128@gmail.com>
2026-06-20 21:17:41 +0530

    added search service for elastic search

---
+++ search-service/src/test/java/com/cfs/searchservice/SearchServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.cfs.searchservice;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
