8ee24b55758a1833269f68411796b470982b331f
Aman Gautam <amangautam2128@gmail.com>
2026-06-20 23:57:14 +0530

    Added chat service

---
+++ chat-service/src/test/java/com/cfs/chatservice/ChatServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.cfs.chatservice;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
